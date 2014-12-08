7ade56e4d25e84050de14726cc729ae61d771e8c
nigel <nigel@webbox.co.za>
2014-12-08 12:31:53 +0200

    news ticker working

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
   get 'gallery/index'
   get 'home/index'
   get 'home/about_us'
-
+  mount Uploader::Engine => '/uploader'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
