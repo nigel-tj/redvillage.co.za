8985a306168c14b6a086c46d997ea8237a2a63ef
nigel <hypej.10@gmail.com>
2015-04-30 17:41:09 +0200

    1 day before launch

--- config/routes.rb
+++ config/routes.rb
@@ -21,17 +21,24 @@ Rails.application.routes.draw do
   get 'home/about_us'
   get 'home/calendar'
   get 'user/index'
-
+  get 'home/lifestyle'
+  get 'home/videos'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   match '/index' => 'home#index', :as => 'index', :via => :get
-  match 'about' => 'home#about_us', :as => 'about', :via => :get
+  match '/about' => 'home#about_us', :as => 'about', :via => :get
   match 'gallery' => 'gallery#index', :as => 'gallery', :via => :get
-  match 'calendar'   => 'home#calendar', :via => :get
+  match '/calendar'   => 'home#calendar', :via => :get
   match 'admins' => 'admins#index', :via => :get
   match 'uploads' => 'uploads#new', :via => :get
   match 'user' => 'user#index', :via => :get
+  match '/store' => 'home#store', :via => :get
+  match '/trends' => 'home#trends', :via => :get
+  match '/music' => 'home#music', :via => :get
+  match '/lifestyle' => 'home#lifestyle', :via => :get
+  match '/contact' => 'home#contact', :via => :get
+  match '/videos' => 'home#videos', :via => :get
   # You can have the root of your site routed with "root"
   root 'home#index'
   #root to: 'documents#new'
