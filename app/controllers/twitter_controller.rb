9da572d52bfd1d4b99da1c72efd09bfb6990ecea
john  kyony <jkyony@gmail.com>
2015-05-01 13:20:39 +0000

    the git fix

--- app/controllers/twitter_controller.rb
+++ app/controllers/twitter_controller.rb
@@ -1,3 +1,14 @@
 class TwitterController < ApplicationController
     before_filter :authenticate_user
+    
+    def generate_twitter_oauth_url
+    	oauth_callback = "http://#{request.host}:#{request.port}/oauth_account"
+    
+    	@consumer = OAuth::Consumer.new("your-consumer-key","your-consumer-secret", :site => "https://api.twitter.com")
+    
+              @request_token = @consumer.get_request_token(:oauth_callback => oauth_callback)
+     	session[:request_token] = @request_token
+    
+    	redirect_to @request_token.authorize_url(:oauth_callback => oauth_callback)
+    end
 end
